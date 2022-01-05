dcb4024ea0c2b5390521b75b651e70b4f5f0b5da
Tobias Wulsdorf <tobias.wulsdorf@uni-marburg.de>
2022-01-04 16:07:16 -0800

    Made the whole thing a module

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.1"
+
+setup(
+    name='xtalmdscripts',
+    author='Tobias Huefner',
+    author_email='thuefner@health.ucsd.edu',
+    description='A collection of scripts useful for the xtalmd project \
+in the group of Mike Gilson at UC San Diego.',
+    version=__version__,
+    license='MIT',
+    platforms=['Linux'],
+    zip_safe=False,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'make_supercell = xtalmdscripts.supercellbuilding.make_supercell:entry_point',
+            'make_tables    = xtalmdscripts.tableformatting.make_tables:entry_point',
+            ]
+        },
+    )
