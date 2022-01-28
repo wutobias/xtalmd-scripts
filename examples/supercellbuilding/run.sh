16fde95268ea1f1f601a07a087d5f4cbdcdb6630
Tobias Huefner <thuefner@ucsd.edu>
2022-01-28 12:46:52 -0800

    Removed run.sh from examples

--- examples/supercellbuilding/run.sh
+++
@@ -1,5 +0,0 @@
-# Default 3x3x3 lattice
-make_supercell -i 1101307.cif -o 1101307.pdb
-# 5x5x5 lattice
-make_supercell -i 1101307.cif -o 1101307.pdb --a_min_max -2 2 --b_min_max -2 2 --c_min_max -2 2
-
