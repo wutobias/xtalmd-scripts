b9d0911eee0f1aa84d5916c4b7847fb5035e8b5f
Tobias Huefner <thuefner@ucsd.edu>
2022-02-04 14:50:07 -0800

    Added run_xtal_md script

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ in the group of Mike Gilson at UC San Diego.',
             'make_tables    = xtalmdscripts.tableformatting.make_tables:entry_point',
             'build_system   = xtalmdscripts.buildsystem.build_system:entry_point',
             'run_xtal_min   = xtalmdscripts.modelling.run_min:entry_point',
+            'run_xtal_md    = xtalmdscripts.modelling.run_md:entry_point',
             ]
         },
     include_package_data=True
