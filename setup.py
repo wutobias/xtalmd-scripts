14d11687394cbe0338f90de71d988a12e15fa72a
Tobias Huefner <thuefner@ucsd.edu>
2022-02-04 12:41:08 -0800

    Add new scripts

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ in the group of Mike Gilson at UC San Diego.',
             'make_supercell = xtalmdscripts.supercellbuilding.make_supercell:entry_point',
             'make_tables    = xtalmdscripts.tableformatting.make_tables:entry_point',
             'build_system   = xtalmdscripts.buildsystem.build_system:entry_point',
+            'run_xtal_min   = xtalmdscripts.modelling.run_min:entry_point',
             ]
         },
     include_package_data=True
