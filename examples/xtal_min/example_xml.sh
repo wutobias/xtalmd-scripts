ad4a506db1ad8cf65724c23da6fc83ebf5c40b96
Tobias Huefner <thuefner@ucsd.edu>
2022-02-04 15:29:28 -0800

    Changed output to prefix in argparse

--- examples/xtal_min/example_xml.sh
+++ examples/xtal_min/example_xml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-run_xtal_min xml -i ../build_system/oxamide/1473522_gaff1.xml -p ../build_system/oxamide/1473522_gaff1.pdb -o state_min --steps 2
+run_xtal_min xml -i ../build_system/oxamide/1473522_gaff1.xml -p ../build_system/oxamide/1473522_gaff1.pdb -pre 1473522_gaff1_min --steps 2
