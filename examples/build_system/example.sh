67422c6cf1d4fdd37af677bc2ba87117e137c481
Tobias Huefner <thuefner@ucsd.edu>
2022-02-04 23:56:49 -0800

    Added examples

--- examples/build_system/example.sh
+++ examples/build_system/example.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-build_system -i ../data/oxamide/1473522.cif -o oxamide/1473522_gaff1.xml -ff gaff1
-build_system -i ../data/oxamide/1473522.cif -o oxamide/1473522_gaff2.xml -ff gaff2
-build_system -i ../data/adipamide/1101307.cif -o adipamide/1101307_gaff1.xml -ff gaff1
-build_system -i ../data/adipamide/1101307.cif -o adipamide/1101307_gaff2.xml -ff gaff2
+toppar="/nfs/data_onsager/projects/xtalmd/toppar_c36_jul21"
+
+build_system -i ../data/oxamide/1473522.cif   -pre oxamide/1473522_gaff1   -ff gaff1
+build_system -i ../data/oxamide/1473522.cif   -pre oxamide/1473522_gaff2   -ff gaff2
+build_system -i ../data/oxamide/1473522.cif   -pre oxamide/1473522_parsley -ff parsley
+build_system -i ../data/oxamide/1473522.cif   -pre oxamide/1473522_sage    -ff sage
+build_system -i ../data/oxamide/1473522.cif   -pre oxamide/1473522_cgenff  -ff cgenff --toppar ${toppar}
+build_system -i ../data/oxamide/1473522.cif   -pre oxamide/1473522_oplsaa  -ff oplsaa
+
+build_system -i ../data/adipamide/1101307.cif   -pre adipamide/1101307_gaff1   -ff gaff1
+build_system -i ../data/adipamide/1101307.cif   -pre adipamide/1101307_gaff2   -ff gaff2
+build_system -i ../data/adipamide/1101307.cif   -pre adipamide/1101307_parsley -ff parsley
+build_system -i ../data/adipamide/1101307.cif   -pre adipamide/1101307_sage    -ff sage
+build_system -i ../data/adipamide/1101307.cif   -pre adipamide/1101307_cgenff  -ff cgenff --toppar ${toppar}
+build_system -i ../data/adipamide/1101307.cif   -pre adipamide/1101307_oplsaa  -ff oplsaa
